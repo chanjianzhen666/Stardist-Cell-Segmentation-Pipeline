71b89f4d97a4d7cbb63b1dc827677137e0ff5dd7
Matt <95849174+chanjianzhen666@users.noreply.github.com>
2024-02-23 11:45:34 +0800

    Update imgpre.py

--- imgpre.py
+++ imgpre.py
@@ -7,14 +7,12 @@ from skimage.exposure import equalize_adapthist
 from skimage.filters.thresholding import threshold_otsu
 import scipy.ndimage as ndi
 
-def img_preprocess(img):
+def img_preprocess(img, sigma, open_size):
     # Adjust image contrast by adaptively equalizing the histogram
     img_equalized_adapted = equalize_adapthist(img, kernel_size=None)
 
     img = img_equalized_adapted
     img = (((img - img.min()) / (img.max() - img.min())) * 255).astype('uint8')
-
-    sigma = 1.0
 
     # Apply the filter and allocate the output to a new variable.
     img_smooth = ndi.gaussian_filter(img, sigma)
@@ -23,7 +21,7 @@ def img_preprocess(img):
     thresh = threshold_otsu(img_smooth)
     mem_adapted = img_smooth > thresh
     mem = mem_adapted
-    i = 5
+    i = open_size
     SE = (np.mgrid[:i,:i][0] - np.floor(i/2))**2 + (np.mgrid[:i,:i][1] - np.floor(i/2))**2 <= np.floor(i/2)**2
     pad_size = i + 1
     mem_padded = np.pad(mem, pad_size, mode='reflect')
@@ -33,16 +31,19 @@ def img_preprocess(img):
     return img_final
 
 @click.command()
-@click.option('--mode', '-m', default='d', help="choose processing mode, 'f' for a sigle file, 'd' for all files under the directory")
+@click.option('--mode', '-m', default='d', help="Choose processing mode, 'd' for all files under the directory (default), 'f' for a sigle file")
+@click.option('--sigma', '-s', default=1.0, help="Gaussian filter argument")
+@click.option('--open_size', '-o', default=5, help="Open operation argument")
 @click.argument('inputpath')
 @click.argument('outputpath')
-def main(mode, inputpath, outputpath):
+def main(mode, sigma, open_size, inputpath, outputpath):
     if mode == 'f':
         store = imread(inputpath, aszarr=True)
         z = zarr.open(store, mode='r')
+        
         if z.name == None:
             img = z
-            img_processed = img_preprocess(img)
+            img_processed = img_preprocess(img, sigma, open_size)
             imwrite(outputpath, img_processed)
             click.echo('Done!')
         
@@ -56,7 +57,7 @@ def main(mode, inputpath, outputpath):
             for i in range(2):
                 img = z[i*height_per_tile:(i+1)*height_per_tile, :, 0]
                 img = (((img - img.min()) / (img.max() - img.min())) * 255).astype('uint8')
-                img_preprocessed = img_preprocess(img)
+                img_preprocessed = img_preprocess(img, sigma, open_size)
                 img_final[i*height_per_tile:(i+1)*height_per_tile, :] = img_preprocessed
 
             imwrite(outputpath, img_final)
@@ -70,8 +71,8 @@ def main(mode, inputpath, outputpath):
         for file in files:
             if str(file).endswith('.tif') or str(file).endswith('.tiff'):
                 img = imread(inputpath + '\\' + file)
-                img_processed = img_preprocess(img)
-                imwrite(outputpath + '\\' + 'pro_' + str(file)[:-5] + '.tiff', img_processed)
+                img_processed = img_preprocess(img, sigma, open_size)
+                imwrite(outputpath + '\\' + str(file), img_processed)
                 click.echo(str(file) + ' done')
         click.echo('All done!')
 
